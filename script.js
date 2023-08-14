e38456d30aa94529d08c002bbb412e3747a5d8ae
Nick Oddo <n.oddo2383@gmail.com>
2023-08-13 21:13:38 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+
+const p1 = document.createElement("p1");
+p1.textContent = "I still dont recognize the importance of adding information through the DOM in JS and not just changing the source HTML for these additions and manipulate them there.";
+document.querySelector("body").appendChild(p1);
